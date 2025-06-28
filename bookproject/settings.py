a345980c01e3181da40ec76544f2d7975843fc3f
neufvilles <ninecities2001@gmail.com>
2025-06-29 02:24:34 +0900

    thrid

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i29^wx&da(_v%#3&iy3qg10c0-72%x^l91wn=vc5-+!2ie=13p"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
